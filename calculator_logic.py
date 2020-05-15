1fa67fe559863f7768096f42ad5ded186ef07db5
s3pt1l3 <45712837+Sept1le@users.noreply.github.com>
2020-05-15 18:31:15 +0300

    Fixed bugs
    Fixed bugs with operators on the beggining of the expression

--- calculator_logic.py
+++ calculator_logic.py
@@ -1,21 +1,46 @@
+from rpn import reverse_polish_notation
+
+
 def push_number(ui, button):
     if ui.cleared:
-        if button.text() != "=" and button.text() != "C" and button.text() != "←":
-            if button.text() in ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]:
-                ui.li.clear()
+        ui.lineEdit.setText('0')
+        if button.text() != "=" and button.text() != "C" and button.text() != "←" and button.text() != '+' \
+                and button.text() != '-' and button.text() != '(' and button.text() != ')' and button.text() != '×'\
+                and button.text() != '÷' and button.text() != '.':
+            if button.text() == '-':
+                ui.li = ''
                 ui.cleared = False
-                ui.li.append(button.text())
-                ui.lineEdit.setText(ui.li[0])
+                ui.li += button.text()
+                ui.lineEdit.setText(ui.li)
+            elif button.text() in ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]:
+                ui.li = ''
+                ui.cleared = False
+                ui.li += button.text()
+                ui.lineEdit.setText(ui.li)
                 for i in ui.li:
                     print(i)
             else:
-
-                ui.li.append(button.text())
+                ui.li += button.text()
                 for i in ui.li:
                     print(i)
 
     else:
         if button.text() != "=" and button.text() != "C" and button.text() != "←":
-            ui.li[0] += button.text()
-            ui.lineEdit.setText(ui.li[0])
+            ui.li += button.text()
+            ui.lineEdit.setText(ui.li)
             print(ui.li)
+        elif button.text() == 'C':
+            ui.li += ''
+            ui.lineEdit.clear()
+            ui.lineEdit.setText('0')
+            ui.cleared = True
+
+        elif button.text() == '←':
+            ui.li = ui.li[0:-1]
+            ui.lineEdit.setText(ui.li)
+            if ui.li == '':
+                ui.lineEdit.setText('0')
+        elif button.text() == '=':
+            result = reverse_polish_notation(ui.li)
+            ui.lineEdit.setText(str(result))
+            ui.cleared = True
