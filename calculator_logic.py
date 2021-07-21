354a7a3b86b35c0203069227d6bee55a3f595f64
Semyon Stepanov <45712837+s3pt1l3@users.noreply.github.com>
2021-07-21 16:18:16 +0300

    Fixed bugs

--- calculator_logic.py
+++ calculator_logic.py
@@ -2,7 +2,7 @@ def push_number(ui, button):
     if ui.cleared:
         ui.lineEdit.setText('0')
         ui.li = '0'
-        if button.text() not in '=C←).':
+        if button.text() not in '=C←)':
             if button.text() in "1234567890-(":
                 ui.li = ''
                 ui.cleared = False
@@ -13,27 +13,32 @@ def push_number(ui, button):
                 ui.cleared = False
                 ui.lineEdit.setText(ui.li)
     else:
-        if ui.li[0] == '0' and len(ui.li) > 2:
-            ui.li = str(eval(ui.li))
         if button.text() != "=" and button.text() != "C" and button.text() != "←" and button.text() != ".":
             if button.text() not in '1234567890-' and ui.li[-1] == '(':
                 pass
             elif button.text() in '+×÷-':
-                if ui.li[-1] in '+-×÷.':
-                    pass
-                else:
+                if ui.li[-1] not in '+-×÷.':
                     ui.li += button.text()
                     ui.lineEdit.setText(ui.li)
             elif button.text() in '()' and ui.li[-1] == '.':
                 pass
             elif button.text() == '(' and ui.li[-1] not in '+-×÷':
                 pass
-            elif button.text() in '1234567890' and ui.li[0] == '0':
-                ui.li = button.text()
-                ui.lineEdit.setText(ui.li)
             else:
-                ui.li += button.text()
-                ui.lineEdit.setText(ui.li)
+                if ui.li[0] == '0' and ui.li[-1] in '+×÷-':
+                    ui.li += button.text()
+                    ui.li = ui.li.replace('×', '*')
+                    ui.li = ui.li.replace('÷', '/')
+                    try:
+                        ui.li = str(eval(ui.li))
+                        ui.lineEdit.setText(ui.li)
+                    except ZeroDivisionError:
+                        ui.lineEdit.setText('Error: division by zero')
+                        ui.li = '0'
+                        ui.cleared = True
+                else:
+                    ui.li += button.text()
+                    ui.lineEdit.setText(ui.li)
         elif button.text() == 'C':
             ui.li += ''
             ui.lineEdit.clear()
@@ -46,10 +51,7 @@ def push_number(ui, button):
                 ui.lineEdit.setText('0')
                 ui.cleared = True
         elif button.text() == '.':
-            print(ui.li)
-            if ui.li[-1] in '+-×÷()':
-                pass
-            else:
+            if ui.li[-1] not in '+-×÷().':
                 ui.li += button.text()
                 ui.lineEdit.setText(ui.li)
         elif button.text() == '=':
@@ -71,26 +73,25 @@ def push_number(ui, button):
                             ui.cleared = True
                             ui.dots = True
                             break
-                    if ui.dots:
-                        pass
-                    else:
+                    if not ui.dots:
                         ui.li = ui.li2
                         try:
                             ui.li = ui.li.replace('×', '*')
                             ui.li = ui.li.replace('÷', '/')
                             result = eval(ui.li)
                             ui.lineEdit.setText(str(result))
-                            ui.cleared = True
+                            ui.li = str(result)
                         except ZeroDivisionError:
                             ui.lineEdit.setText('Error: division by zero')
-                            ui.cleared = True
+                            ui.li = str(result)
                 else:
                     try:
                         ui.li = ui.li.replace('×', '*')
                         ui.li = ui.li.replace('÷', '/')
                         result = eval(ui.li)
                         ui.lineEdit.setText(str(result))
-                        ui.cleared = True
+                        ui.li = str(result)
                     except ZeroDivisionError:
                         ui.lineEdit.setText('Error: division by zero')
+                        ui.li = '0'
                         ui.cleared = True
